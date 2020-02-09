957e5be564a2f2a1e675a356fd90ad54659be753
Felipe Santos <felipessantos.ti@gmail.com>
2020-02-09 00:56:31 -0300

    Melhoria em template do ingresso

--- resources/views/admin/tickets/print.blade.php
+++ resources/views/admin/tickets/print.blade.php
@@ -1,7 +1,7 @@
 <?php namespace ThunderByte\Http\Controllers;
 use Picqer;
 ?>
-<!doctype html>
+    <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,28 +9,48 @@ use Picqer;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <style type="text/css">
+        @page {
+            margin: 100px 10px 40px 10px;
+        }
+
+        #head {
+            background-repeat: no-repeat;
+            text-align: center;
+            height: 110px;
+            width: 100%;
+            position: fixed;
+            top: -100px;
+            left: 0;
+            right: 0;
+            margin: auto;
+        }
+
+        #corpo {
+            width: 600px;
+            position: relative;
+            margin: auto;
+        }
+
+        #footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+        }
+
+    </style>
 
     <title>Ingresso</title>
 </head>
 <body>
-<div class="align-center">
-    <p><?= $title?></p>
-    <br/>
-    <br/>
-    <br/>
-    <p>Conteúdo Livre</p>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-</div>
+<div id="head"><?= $title?></div>
 
+<div id="footer">
 <?php
 $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
 echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($numero, $generator::TYPE_CODE_128)) . '">';
 ?>
-
+</div>
 </body>
 </html>
